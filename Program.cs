9e3281b3752e73e7c541cd27109afec06364e301
Greg Doud <gpdoud@gmail.com>
2022-03-26 18:02:52 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.SqlClient;
+var connStr = @"server=ripper\sqlexpress;database=Test;uid=dsi;pwd=dsiadmin;TrustServerCertificate=True";
+var conn = new SqlConnection(connStr);
+conn.Open();
+if(conn.State != System.Data.ConnectionState.Open) {
+    throw new Exception("Connection failed!");
+}
+System.Console.WriteLine("Success!");
+var sql = "Select * from test";
+var cmd = new SqlCommand(sql, conn);
+var reader = cmd.ExecuteReader();
+while(reader.Read()) {
+    var text = reader["text"].ToString();
+    System.Console.WriteLine($"Text is {text}");
+}
+reader.Close();
+conn.Close();
